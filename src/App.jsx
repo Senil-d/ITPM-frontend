f240f6c9c51586b6bb14ce26cc006857f2e68648
Senil-d <dimalkawijesundara@gmail.com>
2024-03-19 19:39:12 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 import ListYourTour from './pages/ListYourTour';
+import DisplayTours from './pages/DisplayTours';
+import SignIn from './pages/SignIn'
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+   <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/landing-page" element={<LandingPage/>}/>
       <Route path="/list-your-trip" element={<ListYourTour/>}/>
+      <Route path="/display-tours" element={<DisplayTours/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
     </Routes>
   </BrowserRouter>
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-gray-900'>
+     <div className='justify-between flex items-center max-w-6xl mx-auto p-3'>
+
+       {/* web site name */}
+       <Link to='/landing-page'>
+       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap justify-start flex-1'>
+         <span className='text-slate-100'>TRAVE</span>
+         <span className='text-slate-700'>RSE</span>
+       </h1>
+       </Link>
+
+       {/* Search bar */}
+       <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+         <input type='text'placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+         <FaSearch className='text-slate-600'/> 
+       </form>
+
+       {/* Navigation bar */}
+       <ul className='flex gap-6'>
+         <Link to='/'>
+           <li className='hidden sm:inline text-white font-semibold hover:underline'>Home</li>
+         </Link>
+         <Link to='/display-tours'>
+           <li className='hidden sm:inline text-white font-semibold hover:underline'>Tour Packages</li>
+         </Link>
+         <Link to='/list-your-trip'>
+           <li className='hidden sm:inline text-white font-semibold hover:underline'>List Your Trip</li>
+         </Link>
+         <Link to='/sign-in'>
+           <li className='text-white font-semibold hover:underline'>Sign in</li>
+         </Link>
+       </ul>
+       
+     </div>
+    </header>
+  )
+}
