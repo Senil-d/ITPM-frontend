4b642df1e189b110fbb317552da8cadf85ce1cb8
Senil-d <dimalkawijesundara@gmail.com>
2024-03-22 20:12:11 +0530

    create supplier sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import LandingPage from './pages/LandingPage';
-import ListYourTour from './pages/ListYourTour';
-import DisplayTours from './pages/DisplayTours';
-import SignIn from './pages/SignIn'
+import Home from './pages/Home.jsx';
+import LandingPage from './pages/LandingPage.jsx';
+import ListYourTour from './pages/ListYourTour.jsx';
+import DisplayTours from './pages/DisplayTours.jsx';
+import SupplierSignIn from './pages/SupplierSignIn.jsx';
+import SupplierSignUp from './pages/SupplierSignUp.jsx';
 import Header from './components/Header';
 
 export default function App() {
@@ -14,7 +15,8 @@ export default function App() {
       <Route path="/landing-page" element={<LandingPage/>}/>
       <Route path="/list-your-trip" element={<ListYourTour/>}/>
       <Route path="/display-tours" element={<DisplayTours/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/supplier-signin" element={<SupplierSignIn/>}/>
+      <Route path="/supplier-signup" element={<SupplierSignUp/>}/>
     </Routes>
   </BrowserRouter>
 }
---
+++ src/pages/SupplierSignIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SupplierSignIn() {
+  return (
+    <div>
+      Supplier Sign In
+    </div>
+  )
+}
+
