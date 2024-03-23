8b8a1df6bb8b45dfd94ac5b0614bb8ed4981da2f
Senil-d <dimalkawijesundara@gmail.com>
2024-03-23 17:06:57 +0530

    create separate header component for supplier

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import ListYourTour from './pages/ListYourTour.jsx';
@@ -6,18 +6,37 @@ import DisplayTours from './pages/DisplayTours.jsx';
 import SupplierSignIn from './pages/SupplierSignIn.jsx';
 import SupplierSignUp from './pages/SupplierSignUp.jsx';
 import Header from './components/Header';
+import SupplierHeader from './components/SupplierHeader.jsx';
+
 
 export default function App() {
-  return <BrowserRouter>
-   <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/landing-page" element={<LandingPage/>}/>
-      <Route path="/list-your-trip" element={<ListYourTour/>}/>
-      <Route path="/display-tours" element={<DisplayTours/>}/>
-      <Route path="/supplier-signin" element={<SupplierSignIn/>}/>
-      <Route path="/supplier-signup" element={<SupplierSignUp/>}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
 }
 
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      {(location.pathname !== '/list-your-trip' && 
+        location.pathname !== '/supplier-signin' &&
+        location.pathname !== '/supplier-signup') && <Header />}
+      {(location.pathname === '/list-your-trip' ||
+        location.pathname === '/supplier-signin' ||
+        location.pathname === '/supplier-signup') && <SupplierHeader />}
+        
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/list-your-trip" element={<ListYourTour />} />
+        <Route path="/display-tours" element={<DisplayTours />} />
+        <Route path="/supplier-signin" element={<SupplierSignIn />} />
+        <Route path="/supplier-signup" element={<SupplierSignUp />} />
+      </Routes>
+    </>
+  );
+}
--- src/components/SupplierHeader.jsx
+++ src/components/SupplierHeader.jsx
@@ -1,10 +1,10 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 
-export default function Header() {
+export default function SupplierHeader() {
   return (
     <header className='bg-gray-900'>
-     <div className='justify-between flex items-center max-w-6xl mx-auto p-3'>
+     <div className='justify-between flex items-center max-w-6xl mx-auto p-4'>
 
        {/* web site name */}
        <Link to='/landing-page'>
@@ -14,24 +14,16 @@ export default function Header() {
        </h1>
        </Link>
 
-       {/* Search bar */}
+       {/* Search bar
        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
          <input type='text'placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
          <FaSearch className='text-slate-600'/> 
-       </form>
+       </form> */}
 
        {/* Navigation bar */}
        <ul className='flex gap-6'>
-         <Link to='/'>
-           <li className='hidden sm:inline text-white font-semibold hover:underline'>Home</li>
-         </Link>
-         <Link to='/display-tours'>
-           <li className='hidden sm:inline text-white font-semibold hover:underline'>Tour Packages</li>
-         </Link>
-         <Link to='/list-your-trip'>
-           <li className='hidden sm:inline text-white font-semibold hover:underline'>List Your Trip</li>
-         </Link>
-         <Link to='/sign-in'>
+         
+         <Link to='/supplier-signin'>
            <li className='text-white font-semibold hover:underline'>Sign in</li>
          </Link>
        </ul>
